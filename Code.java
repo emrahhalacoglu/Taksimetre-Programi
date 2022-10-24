0fe515c7213b873bec83646106baa075e7ce3b3e
ehm <emrahhalacoglu@gmail.com>
2022-10-24 14:32:26 +0300

    Scanner warning fixed.

--- Code.java
+++ Code.java
@@ -5,9 +5,10 @@ public class Code {
         baslangicTutari=10;
         kilometreTutari=2.20;
         minimumOdeme=20;
-        Scanner input=new Scanner(System.in);
-        System.out.print("Mesafe Giriniz :");
-        mesafe=input.nextDouble();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Mesafe Giriniz :");
+            mesafe=input.nextDouble();
+        }
         taksimetreTurari=baslangicTutari+mesafe*kilometreTutari;
         taksimetreTurari=(taksimetreTurari<20)?minimumOdeme:taksimetreTurari;
         System.out.println("Taksimetre Tutari :"+taksimetreTurari+" TL");
